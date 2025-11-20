3fa52fe39333b3d8c83c952c50d33da4a72d3519
Spectra <shaikhfujail4444@gmail.com>
2025-11-20 12:05:51 +0530

    next.config file path change

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-static-site",          // 👈 subfolder path
-  assetPrefix: "/my-static-site/",      // 👈 ensures _next assets load correctly
+  basePath: "/PSC-Engineering-Website",          // 👈 subfolder path
+  assetPrefix: "/PSC-Engineering-Website/",      // 👈 ensures _next assets load correctly
   trailingSlash: true,                  // 👈 optional, makes URLs end with /
   images: {
     unoptimized: true,                  // disables server-side image optimization
